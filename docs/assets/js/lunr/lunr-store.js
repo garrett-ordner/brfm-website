338f949f541c78d5807c04d825365b90b36f30d3
garrett-ordner <garrettordner@gmail.com>
2025-06-10 22:55:48 -0500

    finally fixed the pathing for assets

--- docs/assets/js/lunr/lunr-store.js
+++ docs/assets/js/lunr/lunr-store.js
@@ -2,18 +2,18 @@ var store = [{
         "title": "About our meeting",
         "excerpt":"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat...","categories": [],
         "tags": [],
-        "url": "/about/",
+        "url": "/brfm-website/about/",
         "teaser": null
       },{
         "title": "Contact Us",
         "excerpt":"You can reach us at placeholder@placeholder.org or call 555-555-5555.  ","categories": [],
         "tags": [],
-        "url": "/contact/",
+        "url": "/brfm-website/contact/",
         "teaser": null
       },{
         "title": "Meeting for Worship",
         "excerpt":"We gather for worship each First Day (Sunday) at 11:30 AM, followed by fellowship. All are welcome. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex...","categories": [],
         "tags": [],
-        "url": "/worship/",
+        "url": "/brfm-website/worship/",
         "teaser": null
       },]
